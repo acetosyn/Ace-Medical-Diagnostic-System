487ccd3fc7dd5b6a784d8b74c4062e73bf933e2e
ACETOSYN <64759470+acetosyn@users.noreply.github.com>
2025-07-12 17:12:16 +0100

    Updated several pages

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder= 'templates')
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
+
+@app.route("/how")
+def how():
+    return render_template("how.html")
+
+@app.route("/clinic")
+def clinic():
+    return render_template("clinic.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/login")
 def login():
@@ -18,4 +34,3 @@ def register():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000, debug= True)
-
